f4271e602029da99c81010ca9767168a54ccdba9
felipe <felipematosapps@gmail.com>
2024-09-22 18:06:40 -0300

    segundo commit

--- app/src/main/java/com/example/appquiz/MainActivity.kt
+++ app/src/main/java/com/example/appquiz/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.appquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editNome : EditText
+    private lateinit var btnIniciar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        editNome = findViewById(R.id.edit_nome)
+        btnIniciar = findViewById(R.id.btn_iniciar)
+
+        btnIniciar.setOnClickListener{
+
+            val nome = editNome.text.toString()
+
+            val intent = Intent(this,PerguntasActivity::class.java)
+
+            intent.putExtra("nome_usuario",nome)
+
+            startActivity(intent)
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/appquiz/PerguntasActivity.kt
@@ -0,0 +1,16 @@
+package com.example.appquiz
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PerguntasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_perguntas)
+
+    }
+}
